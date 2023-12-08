1071b40a0377e5313b59f254d3c097b1d51054bf
AndressaKarla <andressa.karla1@gmail.com>
2023-12-08 18:09:27 -0300

    Adicionando funcionalidade e cenário em login-entrar.cy.js e config tamanho janela

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1280,
+		viewportHeight: 720,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
       const environment = config.env.environment || 'dev'
--- cypress/e2e/login-entrar.cy.js
+++ cypress/e2e/login-entrar.cy.js
@@ -8,27 +8,29 @@ import seletores from '../support/seletores'
     Para validar o comportamento da funcionalidade
  */
 
-describe('Validar Login usuário administrador', () => {
-  context('Dado que eu acesse a tela de Login do front do ServeRest', () => {
-    before(() => {
-      cy.visit('/login')
-    })
-
-    context('Quando eu informar os campos de email e senha de um usuário administrador', () => {
-      beforeEach(() => {
-        cy.fixture('login').then((loginFixture) => {
-          cy.realizarLoginEntrar(loginFixture.adminValido1.emailValido1, loginFixture.adminValido1.senhaValida1)
-        })
+describe('Funcionalidade: Tela Login - Botão Entrar', () => {
+  context('Cenário: Validar Login usuário administrador', () => {
+    context('Dado que eu acesse a tela de Login do front do ServeRest', () => {
+      before(() => {
+        cy.visit('/login')
       })
 
-      it('Então deverá apresentar a tela Home com o texto Bem Vindo e com o texto Este é seu sistema para administrar seu ecommerce', () => {
-        cy.get(seletores.HOME.TEXTO_BEM_VINDO)
-          .should('be.visible')
-          .should('contain', 'Bem Vindo')
+      context('Quando eu informar os campos de email e senha de um usuário administrador', () => {
+        beforeEach(() => {
+          cy.fixture('login').then((loginFixture) => {
+            cy.realizarLoginEntrar(loginFixture.adminValido1.emailValido1, loginFixture.adminValido1.senhaValida1)
+          })
+        })
 
-        cy.get(seletores.HOME.TEXTO_SISTEMA_ADMINISTRAR_ECOMMERCE)
-          .should('be.visible')
-          .should('contain', 'Este é seu sistema para administrar seu ecommerce.')
+        it('Então deverá apresentar a tela Home com o texto Bem Vindo e com o texto Este é seu sistema para administrar seu ecommerce', () => {
+          cy.get(seletores.HOME.TEXTO_BEM_VINDO)
+            .should('be.visible')
+            .should('contain', 'Bem Vindo')
+
+          cy.get(seletores.HOME.TEXTO_SISTEMA_ADMINISTRAR_ECOMMERCE)
+            .should('be.visible')
+            .should('contain', 'Este é seu sistema para administrar seu ecommerce.')
+        })
       })
     })
   })
